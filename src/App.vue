d023f22742436e750eaaaddd0c08e2e14042a90f
星野梦美 <174050379@qq.com>
2023-10-17 13:49:29 +0800

    feat: 页面bar设定表更

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,56 @@
  * @Author: 星野梦美 222736
  * @Date: 2023-08-10 18:23:53
  * @LastEditors: 174050379@qq.com
- * @LastEditTime: 2023-09-19 15:19:08
+ * @LastEditTime: 2023-09-28 17:28:55
 -->
 <template>
   <div class="app_main_style">
+    <div class="header">
+      <div class="img_box">
+        <img src="aaaa" alt="" />
+      </div>
+      <el-button class="btn_header">
+        <el-icon class="el-icon--right"><ArrowRight />111</el-icon>
+      </el-button>
+    </div>
     <!-- 主页视图 -->
-    <router-view />
-    <el-row class="bar_style">
-      <el-col :span="8" v-for="(item, index) in this.routerTemp" :key="index">
-        <div class="bar_item_style">
-          <router-link :to="item.path">{{ item.name }}</router-link>
-        </div>
-      </el-col>
-    </el-row>
+    <router-view class="body_style" />
+    <!-- 菜单 -->
+    <div class="bar_style">
+      <div class="i_style"></div>
+      <div v-for="(item, index) in this.routerTemp" :key="index">
+        <router-link :to="item.path"
+          ><div class="bar_item_style">{{ item.name }}</div></router-link
+        >
+        <div class="item_i_style"></div>
+      </div>
+    </div>
+    <!-- 抽屉 -->
+    <!-- <el-drawer style="z-index:99999;" v-model="drawer" title="I am the title" direction="rtl" :before-close="handleClose">
+      <span>Hi, there!</span>
+    </el-drawer> -->
   </div>
 </template>
 <script>
+import {
+  ArrowLeft,
+  ArrowRight,
+  Delete,
+  Edit,
+  Share,
+} from '@element-plus/icons-vue'
 import Watermark from './common/js/watermark';
 import router from './router';
 export default {
   name: 'App',
-  components: {},
+  components: {
+    ArrowLeft,
+    ArrowRight
+  },
   data() {
     return {
       routerTemp: [],
+      drawer: false,
     };
   },
   methods: {},
@@ -41,25 +67,61 @@ export default {
 .app_main_style {
   width: 100vw;
   height: 100vh;
-  .bar_style {
-    height: 10vh;
+  position: relative;
+  background: #6b6b6b;
+
+  .header {
+    height: 6vh;
     width: 100vw;
-    // background: pink;
-    position: fixed;
-    bottom: 0;
-    .el-col {
-      border-radius: 4px;
-      line-height: 10vh;
+    z-index: 9999;
+    position: sticky;
+    overflow: hidden;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    .img_box {
+      width: 20vw;
+      height: 4vh;
+      margin-top: 1vh;
+      margin-left: 1vw;
+      background: yellow;
     }
-    .bar_item_style {
-      border-radius: 4px;
-      min-height: 36px;
-      text-align: center;
+    .btn_header {
+      margin-top: 1vh;
+      margin-right: 1vw;
+      // float: right;
     }
   }
-}
-* {
-  margin: 0;
-  padding: 0;
+  .body_style {
+    padding-top: 40px;
+    padding-left: 20px;
+  }
+  .bar_style {
+    position: absolute;
+    top: 6%;
+    left: 2%;
+    .i_style {
+      width: 1px;
+      height: 300px;
+      background: #fff;
+      margin: 20px;
+      position: relative;
+    }
+    .item_i_style {
+      width: 1px;
+      height: 40px;
+      background: #fff;
+      margin: 20px;
+      position: relative;
+    }
+    .bar_item_style {
+      color: whitesmoke;
+      font-size: 14px;
+    }
+    .bar_item_style:hover {
+      color: whitesmoke;
+      font-size: 16px;
+    }
+  }
 }
 </style>
--- src/pages/HOME.vue
+++ src/pages/HOME.vue
@@ -2,18 +2,73 @@
  * @Author: 星野梦美 222736
  * @Date: 2023-08-30 16:09:53
  * @LastEditors: 174050379@qq.com
- * @LastEditTime: 2023-09-18 17:08:05
+ * @LastEditTime: 2023-09-28 16:28:08
 -->
 <!-- 主页 -->
 <template>
-  <div>
-    <h1>Home</h1>
-    <p>Welcome to the homepage!</p>
+  <div style="padding: 20px">
+    <el-carousel :interval="4000" type="card" :autoplay="false">
+      <el-carousel-item v-for="(item, index) in imgList" :key="index">
+        <el-image :src="item.url" />
+      </el-carousel-item>
+    </el-carousel>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-}
+  data() {
+    return {
+      imgList: [
+        {
+          url: 'https://img.zcool.cn/community/01xa8n4rthlg1zhguebfek3538.png?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/quality,q_100/format,webp',
+          name: 'no1',
+        },
+        {
+          url: 'https://img.zcool.cn/community/01qb3qgtdcp6uq9vxcbtpn3236.png?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/quality,q_100/format,webp',
+          name: 'no2',
+        },
+        // {
+        //   url: 'https://img.zcool.cn/community/01ss9dybsuwm9hpsemue713834.png?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/quality,q_100/format,webp',
+        // },
+        // {
+        //   url: 'https://img.zcool.cn/community/01ss9dybsuwm9hpsemue713834.png?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/quality,q_100/format,webp',
+        // },
+        // {
+        //   url: 'https://img.zcool.cn/community/015cnxezseptt0and4sjtv3933.png?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/quality,q_100/format,webp',
+        // },
+        // {
+        //   url: 'https://img.zcool.cn/community/01gjxvpnulp1c7ob8abgdb3838.png?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/quality,q_100/format,webp',
+        // },
+        // {
+        //   url: 'https://img.zcool.cn/community/01jkuxdgtwrhujvoalm44i3333.png?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/quality,q_100/format,webp',
+        // },
+        // {
+        //   url: 'https://img.zcool.cn/community/01mymamozigjivzva5w28j3934.png?x-oss-process=image/auto-orient,1/resize,m_lfit,w_1280,limit_1/sharpen,100/quality,q_100/format,webp',
+        // },
+      ],
+    };
+  },
+  methods: {
+    queryimgindex(e) {
+      console.log(e, '>>ee');
+    },
+  },
+};
 </script>
+<style scoped lang="less">
+.el-carousel__item {
+  height: 20rem;
+}
+.el-carousel__container {
+  height: 20rem;
+}
+.el-carousel__item:nth-child(2n) {
+  background-color: pink;
+}
+
+.el-carousel__item:nth-child(2n + 1) {
+  background-color: #d3dce6;
+}
+</style>
